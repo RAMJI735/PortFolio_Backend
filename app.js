5864c339600f0fc06874dc9741584a08fc6142ca
RAMJI735 <147968785+RAMJI735@users.noreply.github.com>
2025-10-28 15:04:55 +0530

    Update CORS origin to specific frontend URL

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: process.env.FRONTEND_URL
+        origin: "https://react-port-folio-pi.vercel.app/"
     }
 ));
 
