9bd9dadce6937929c1442df0e02af5d794479719
RAMJI735 <147968785+RAMJI735@users.noreply.github.com>
2025-10-28 15:12:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: "https://react-port-folio-pi.vercel.app/"
+          origin: "https://react-port-folio-pi.vercel.app"
     }
 ));
 
